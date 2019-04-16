50b32b6c1bf5466e0141899de8e6681b9b7a1513
Evžen Wybitul <wybitul.evzen@gmail.com>
2019-04-16 20:36:50 +0200

    Use vectors instead of lists

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,17 +1,36 @@
 module Lib where
 
 import           Control.Monad                        (foldM, mapM_, replicateM)
-import           Data.List                            (find, group, groupBy,
-                                                       maximumBy, partition,
-                                                       sort, sortOn)
+import           Data.Function                        (on)
+import           Data.List                            (find, group, maximumBy,
+                                                       partition, sort)
 import           Data.Ord                             (Down (..))
 import           Data.Random
 import           Data.Random.Distribution.Exponential (exponential)
+import           Data.Vector                          (Vector, (!), (//))
+import qualified Data.Vector                          as V (enumFromTo, foldr,
+                                                            fromList, head,
+                                                            tail, take, zip,
+                                                            (++))
+import qualified Data.Vector.Algorithms.Intro         as I (partialSortBy)
+import qualified Data.Vector.Generic                  as G (Vector, modify)
 import           Debug.Trace                          (traceShow)
 
-type Generation = [Alignment]
+type Generation = Vector Alignment
 
-type Alignment = [Seq]
+partialSortBy :: (G.Vector v e) => (e -> e -> Ordering) -> Int -> v e -> v e
+partialSortBy f k = G.modify (\v -> I.partialSortBy f v k)
+
+data Alignment = Alignment
+  { alSeqs  :: Vector Seq
+  , alScore :: Double
+  } deriving (Show)
+
+instance Eq Alignment where
+  Alignment {alScore = s1} == Alignment {alScore = s2} = s1 == s2
+
+instance Ord Alignment where
+  Alignment {alScore = s1} `compare` Alignment {alScore = s2} = s1 `compare` s2
 
 data State =
   S (Double, Int, Double)
@@ -19,11 +38,11 @@ data State =
     (Double, Int, Double)
     (Double, Int, Double)
     (Double, Int, Double)
-  deriving (Show, Eq, Ord)
+  deriving (Show, Eq)
 
 data Seq = Seq
   { len         :: Int
-  , aa          :: String
+  , aa          :: Vector Char
   , gaps        :: [Gap]
   , seedGapMean :: Double -- TODO: Move this to alignment or someplace else
   } deriving (Show, Eq)
@@ -36,7 +55,7 @@ getRandomR (a, b) = uniform a b
 runIO :: Alignment -> IO Alignment
 runIO a = do
   result <- runRVar (run a) StdRandom
-  mapM_ putStrLn . fill $ result
+  mapM_ putStrLn . fill . alSeqs $ result
   return result
 
 run :: Alignment -> RVar Alignment
@@ -44,13 +63,13 @@ run a = do
   let state = S (0.2, 0, 0) (0.2, 0, 0) (0.2, 0, 0) (0.2, 0, 0) (0.2, 0, 0)
   startingG <- populate state a
   fin <- doRuns state startingG 2000
-  return $ maximumOn score fin
+  return $ maximum fin
 
-maximumOn :: Ord b => (a -> b) -> [a] -> a
+maximumOn :: Ord b => (a -> b) -> Vector a -> a
 maximumOn f = maximumBy (\a b -> compare (f a) (f b))
 
 populate :: State -> Alignment -> RVar Generation
-populate st a = go 100 []
+populate st a = V.fromList <$> go 100 []
   where
     go 0 acc = return acc
     go n acc = mutate st a >>= \(na, _) -> go (n - 1) (na : acc)
@@ -67,47 +86,49 @@ nextGen :: State -> Generation -> RVar (Generation, State)
 nextGen st g = do
   let top = top5 g
   (als, ns) <- go st 95 []
-  return (als ++ top, newState ns)
+  return (als V.++ top, newState ns)
   where
-    go :: State -> Int -> [Alignment] -> RVar ([Alignment], State)
-    go s 0 acc = return (acc, s)
+    go :: State -> Int -> [Alignment] -> RVar (Generation, State)
+    go s 0 acc = return (V.fromList acc, s)
     go s n acc = do
       best <- tournament g
       (ng, ns) <- mutate s best
       go ns (n - 1) (ng : acc)
 
-top5 :: Generation -> [Alignment]
-top5 = take 5 . sortOn (Down . score)
+top5 :: Generation -> Vector Alignment
+top5 = V.take 5 . partialSortBy (compare `on` Down) 5
 
 tournament :: Generation -> RVar Alignment
 tournament g = do
   indices <- replicateM 5 $ getRandomR (0, length g - 1)
-  return $ head $ sortOn (Down . score) $ map (g !!) indices
+  return $ maximumBy (compare `on` Down) $ (g !) <$> indices
 
-score :: Alignment -> Double
+score :: Vector Seq -> Double
 score al =
   1000 * meanColumnHomogenicity al + 2000 * gapBlocks al -
   2 * columnsIncrement al
 
-meanColumnHomogenicity :: Alignment -> Double
+meanColumnHomogenicity :: Vector Seq -> Double
 meanColumnHomogenicity al =
-  mean $ map (columnHomogenicity (fill al)) [0 .. len (head al) - 1]
+  mean $ map (columnHomogenicity (fill al)) [0 .. len (V.head al) - 1]
   where
     mean :: [Double] -> Double
     mean ns = sum ns / fromIntegral (length ns)
 
-fill :: Alignment -> [String]
-fill al = fill' (maximum $ map (\s -> len s + sum (map snd (gaps s))) al) al
+fill :: Vector Seq -> [String]
+fill al = fill' (maximum $ fmap (\s -> len s + sum (fmap snd (gaps s))) al) al
   where
-    fill' _ [] = []
-    fill' n (x:xs) =
-      let res = go x
-          l = length res
-       in (if l < n
-             then res ++ replicate (n - l) '-'
-             else res) :
-          fill' n xs
-    go Seq {aa = aa, len = _, gaps = gs} = foldr (helper gs) "" (zip aa [1 ..])
+    fill' n xs
+      | null xs = []
+      | otherwise =
+        let res = go (V.head xs)
+            l = length res
+         in (if l < n
+               then res ++ replicate (n - l) '-'
+               else res) :
+            fill' n (V.tail xs)
+    go Seq {aa = aa, len = _, gaps = gs} =
+      V.foldr (helper gs) "" (V.zip aa $ V.enumFromTo 1 (length aa))
     helper :: [Gap] -> (Char, Int) -> String -> String
     helper gs (c, i) acc =
       case find ((== i) . fst) gs of
@@ -129,40 +150,40 @@ columnHomogenicity al n = fromIntegral numerator / fromIntegral denominator
     countWithoutGaps xs      = length xs
     sqr x = x * x
 
-gapBlocks :: Alignment -> Double
+gapBlocks :: Vector Seq -> Double
 gapBlocks seqs =
   if gb == 0
     then 0
     else 1 / gb
   where
-    gb = sum (map (fromIntegral . length . gaps) seqs)
+    gb = V.foldr (+) 0 (fmap (fromIntegral . length . gaps) seqs)
 
-columnsIncrement :: Alignment -> Double
+columnsIncrement :: Vector Seq -> Double
 columnsIncrement seqs =
   if mSeq <= 1
     then 0
     else fromIntegral newMax / fromIntegral mSeq - 1
   where
-    mSeq = maximum $ map len seqs
-    newMax = maximum $ map (\s -> len s + sum (map snd (gaps s))) seqs
+    mSeq = maximum $ fmap len seqs
+    newMax = maximum $ fmap (\s -> len s + sum (map snd (gaps s))) seqs
 
+-- TODO: Implemen 1.2 mutation probability
 mutate :: State -> Alignment -> RVar (Alignment, State)
-mutate st@(S a b c d e) al = do
+mutate st@(S a b c d e) al@Alignment {alSeqs = seqs, alScore = oldScore} = do
   check <- getRandomR (1, 10)
   if check >= 2
     then do
       p <- replicateM 1 stdUniform
-      let oldScore = score al
       let opIndices = map (pick st) p
       let op = (ops !!) <$> opIndices
       let sts = (ps !!) <$> opIndices
-      seqIndex <- getRandomR (0, length al - 1)
-      let s = al !! seqIndex
+      seqIndex <- getRandomR (0, length seqs - 1)
+      let s = seqs ! seqIndex
       newS <- foldM (\acc o -> o acc) s op
-      let newA = take seqIndex al ++ [newS] ++ drop (seqIndex + 1) al
-      let newScore = score newA
+      let newSeqs = seqs // [(seqIndex, newS)]
+      let newScore = score newSeqs
       let newSt = go st sts opIndices (newScore - oldScore)
-      return (newA, newSt)
+      return (Alignment newSeqs newScore, newSt)
     else return (al, st)
   where
     ops = [insert, increase, decrease, delete, shift]
@@ -179,7 +200,7 @@ divideState (S a b c d e) = [a, b, c, d, e]
 updateMutation :: State -> (Double, Int, Double) -> Int -> State
 updateMutation st (prob, tot, diff) i =
   let mut = divideState st
-   in createState $ take i mut ++ [(prob, tot + 1, diff)] ++ drop (i + 1) mut
+   in createState $ updateAt i [(prob, tot + 1, diff)] mut
 
 createState :: [(Double, Int, Double)] -> State
 createState [a, b, c, d, e] = S a b c d e
@@ -221,7 +242,7 @@ increase sq = do
   let s = gaps sq
   i <- getRandomR (0, length s - 1)
   let (start, l) = s !! i
-  let gs = take i s ++ [(start, l + 1)] ++ drop (i + 1) s
+  let gs = updateAt i [(start, l + 1)] s
   return $ Seq (len sq) (aa sq) gs (seedGapMean sq)
 
 decrease :: Seq -> RVar Seq
@@ -231,10 +252,13 @@ decrease sq = do
   i <- getRandomR (0, length s - 1)
   let (start, l) = s !! i
   if l - 1 == 0
-    then let newS = take i s ++ drop (i + 1) s
+    then let newS = updateAt i [] s
           in return $ Seq (len sq) (aa sq) newS (seedGapMean sq)
-    else let newS = take i s ++ [(start, l - 1)] ++ drop (i + 1) s
+    else let newS = updateAt i [(start, l - 1)] s
           in return $ Seq (len sq) (aa sq) newS (seedGapMean sq)
+
+updateAt :: Int -> [a] -> [a] -> [a]
+updateAt i new xs = take i xs ++ new ++ drop (i + 1) xs
 
 shift :: Seq -> RVar Seq
 shift s@Seq {gaps = []} = return s
@@ -264,5 +288,5 @@ delete s@Seq {gaps = []} = return s
 delete sq = do
   let s = gaps sq
   i <- getRandomR (0, length s - 1)
-  let newS = take i s ++ drop (i + 1) s
+  let newS = updateAt i [] s
   return $ Seq (len sq) (aa sq) newS (seedGapMean sq)
