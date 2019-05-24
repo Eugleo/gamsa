aa2bcc5b274e1a37f9bfb6f5ef7bd429d64ea64d
Evžen Wybitul <wybitul.evzen@gmail.com>
2019-05-24 14:00:26 +0200

    Add new test cases

--- test/Spec.hs
+++ test/Spec.hs
@@ -7,12 +7,16 @@ import Utils                (mkAlignment)
 
 testAl = mkAlignment ["KMMEEAABBGHGHI", "-MMABGHI", "EAABB-H-HI"]
 
+testAl2 = mkAlignment ["AA--", "--AA"]
+
 main :: IO ()
 main =
   hspec $
-  describe "Hlavní část programu" $
-  it "zvýší původní skóre" $ do
-    let originalScore = aScore testAl
-    result <- runRVar (run defaultConfig testAl) StdRandom
-    let newScore = aScore result
-    originalScore `shouldSatisfy` (>=) newScore
+  describe "Hlavní část programu" $ do
+    it "zvýší původní skóre" $ do
+      let originalScore = aScore testAl
+      result <- runRVar (run defaultConfig testAl) StdRandom
+      let newScore = aScore result
+      originalScore `shouldSatisfy` (>=) newScore
+    it "počítá správně mezery i na konci" $
+      aScore testAl2 `shouldBe` (-6 :: Int)
